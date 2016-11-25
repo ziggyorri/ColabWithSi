857d847458a8916e9c57aaa9195294da9a1798e7
sindripal <sindripa@gmail.com>
2016-11-25 12:06:15 +0000

    Register bugfixes

--- dropDownWidthSass/included/login.php
+++ dropDownWidthSass/included/login.php
@@ -10,7 +10,7 @@
 		foreach ($tafla as $k)
 		{
         	if ($k[0] != $Username){}
-        	elseif ($k[0] == $Username && $k[1] == $Password){$logins = [$k[0] => $k[1]];}
+        	elseif ($k[0] == $Username && $k[1] == $Password){$logins = [$k[0] => $k[1]];break;}
         }
 		if(isset($logins[$Username])&&$logins[$Username]==$Password)
 		{
@@ -30,8 +30,8 @@
 
 		<form action="" method="post" name="Innskra">
 			<h1>Login</h1>
-			<input name="Username" type="text" class="Input" placeholder="Username">
-			<input name="Password" type="password" class="Input" placeholder="Password">
+			<input name="Username" type="text" class="Input" placeholder="Username" required>
+			<input name="Password" type="password" class="Input" placeholder="Password" required>
 			<input name="Submit" type="submit" value="Login" class="Button3">
 		</form>
 
