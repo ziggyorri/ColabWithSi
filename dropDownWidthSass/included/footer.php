8fce0d05b6e958e3a72031bba5e7e964846230e9
sindripal <sindripa@gmail.com>
2016-12-04 22:30:12 +0000

    english fix

--- dropDownWidthSass/included/footer.php
+++ dropDownWidthSass/included/footer.php
@@ -1,8 +1,8 @@
 	<div class="opnun">
-		<h2>Opnunartímar</h2>
-		<div class="meh">Virka Daga: <h3>9:00 - 18:00</h3></div>
+		<h2>Opening hours</h2>
+		<div class="meh">Week day: <h3>9:00 - 18:00</h3></div>
 
-		<div>Helgar: <h3>10:00 - 18:00</h3></div>
+		<div>Weekends: <h3>10:00 - 18:00</h3></div>
 	</div>
 		<div id="mapi">
 			<iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13918.706224510119!2d-21.934276045776542!3d64.14662947364903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48d674d3023a19c7%3A0xdbbf050da40f5d28!2sHarpan%2C+101+Reykjav%C3%ADk!5e0!3m2!1sis!2sis!4v1478866535521" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
